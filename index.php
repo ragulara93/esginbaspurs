27f80c0c0f88161e7536667d9706f1b15fa34810
Perinparatnam <ragulara93@gmail.com>
2015-04-08 14:59:12 +0200

    version prof
    version prof

--- index.php
+++ index.php
@@ -1,107 +1,87 @@
 <?php
-	error_reporting(E_ALL);
-	ini_set("display_errors", 1);
-
-
-	session_start();
-
-
-	require("facebook-php-sdk-v4-4.0-dev/autoload.php");
-
-	use Facebook\FacebookSession; /*on charge tout mais on les initialise pas. Ta inclu la page et maintenetant on l'initialise.*/
-	use Facebook\FacebookRedirectLoginHelper;
-	user Facebook\FacebookRequest;
-
-	//variable défini une seule fois et que l'on peu plus modifier
-	const APP_ID = "1556890417914830";
-	const APP_SECRET = "e58912ad3ae1f534444a84525b8ce791";
-
-	FacebookSession::setDefaultApplication(APP_ID, APP_SECRET);
-
-	$helper = new FacebookRedirectLoginHelper('https://esginbaspurs.herokuapp.com/');
-	// $loginUrl = $helper->getLoginUrl();
-	// Use the login url on a link or button to redirect to Facebook for authentication
-
-	if(isset($_SESSION) && isset($_SESSION['fb_token']) )
-	{
-		$session = $helper->new FacebookSession($_SESSION['fb_token']);
-	}
-	else
-	{
-		$session = $helper->getSessionFromRedirect();
-			// var_dump($session);
-			$token = (string) $session->getAccessToken();
-			$_SESSION['fb_token'] = $token;
-	}
-
-
-
+    error_reporting(E_ALL);
+    ini_set("display_errors", 1);
+    
+    session_start();
+    require "facebook-php-sdk-v4-4.0/autoload.php";
+    use Facebook\FacebookSession;
+    use Facebook\FacebookRedirectLoginHelper;
+    use Facebook\FacebookRequest;
+    
+    const APPID = "1556890417914830";
+    const APPSECRET = "e58912ad3ae1f534444a84525b8ce791";
+    FacebookSession::setDefaultApplication(APPID, APPSECRET);
+    $helper = new FacebookRedirectLoginHelper('https://esginbaspurs.herokuapp.com/');
+    if( isset($_SESSION) &&  isset($_SESSION['fb_token']))
+    {
+      $session  = new FacebookSession($_SESSION['fb_token']);
+    }else
+    {
+      $session = $helper->getSessionFromRedirect();
+    }
 ?>
 
 <!DOCTYPE html>
 <html>
-	<head>
-		<meta charset="utf-8">
-		<title>ESGI NBA SPURS</title>
-		<meta name="description" content="description de ma page"> 
-
-		<script>
-		  window.fbAsyncInit = function() {
-		    FB.init({
-		      appId      : '<?php APP_ID; ?>',
-		      xfbml      : true,
-		      version    : 'v2.3'
-		    });
-		  };
-
-		  (function(d, s, id){
-		     var js, fjs = d.getElementsByTagName(s)[0];
-		     if (d.getElementById(id)) {return;}
-		     js = d.createElement(s); js.id = id;
-		     js.src = "//connect.facebook.net/fr_FR/sdk.js";
-		     fjs.parentNode.insertBefore(js, fjs);
-		   }(document, 'script', 'facebook-jssdk'));
-		</script>
-
-	</head>
-
-	<body>
-		<h2>Projet facebook</h2>
-		
-		<br><br><br>
-		<div
-		  class="fb-like"
-		  data-share="true"
-		  data-width="450"
-		  data-show-faces="true">
-		</div>
-
-		<?php
-
-		if($session)
-		{
-			// Prepare la requête
-			$request = new FacebookRequest($session, 'GET', '/me');
-
-			// Exécute la requête
-			$response = $request->execute();
-
-			// Transforme en graphObject
-			$user = $response->getGraphObject("Facebook\GraphUser");
-
-		}
-		else
-		{
-			$loginUrl = $helper->getLoginUrl();
-			?>
-				<a href="<?php echo $loginUrl; ?>">Se connecter</a>
-			<?php
-		}
-
-		?> 
-
-	</body>
+    <head>
+        <meta charset="UTF-8">
+        <title>Titre de ma page</title>   
+        <meta name="description" content="description de ma page">
+       
+    </head>
+    <body>
+         <script>
+          window.fbAsyncInit = function() {
+            FB.init({
+              appId      : '<?php echo APPID;?>',
+              xfbml      : true,
+              version    : 'v2.3'
+            });
+          };
+          (function(d, s, id){
+             var js, fjs = d.getElementsByTagName(s)[0];
+             if (d.getElementById(id)) {return;}
+             js = d.createElement(s); js.id = id;
+             js.src = "//connect.facebook.net/fr_FR/sdk.js";
+             fjs.parentNode.insertBefore(js, fjs);
+           }(document, 'script', 'facebook-jssdk'));
+        </script>
 
 
+        <h1>Mon application facebook</h1>
 
+        <div
+          class="fb-like"
+          data-share="true"
+          data-width="450"
+          data-show-faces="true">
+        </div>
+        <br>
+        <!-- <div class="fb-comments" data-href="http://skrzypczyk.fr" data-numposts="5" data-colorscheme="light"></div> -->
+        <br>
+
+        <?php
+          if($session)
+          {
+            
+            $token = (string) $session->getAccessToken();
+            $_SESSION['fb_token'] = $token;
+            //Prepare
+            $request = new FacebookRequest($session, 'GET', '/me');
+            //execute
+            $response = $request->execute();
+            //transform la data graphObject
+            $user = $response->getGraphObject("Facebook\GraphUser");
+            echo "<pre>";
+            print_r($user);
+            echo "</pre>";
+        
+          }else{
+            $loginUrl = $helper->getLoginUrl();
+            echo "<a href='".$loginUrl."'>Se connecter</a>";
+          }
+            
+        ?>
+    </body>
 </html>
+
